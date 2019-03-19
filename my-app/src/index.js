fb9daa6e3bd15b3859a8f48b2d8b5cd7d569d453
David Braley <david.braley@gmail.com>
2019-03-18 22:31:26 -0400

    Lift state to the Game component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -73,18 +73,28 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  render () {
     return (
       <div className="game">
-      <div className="game-board">
-      <Board />
+        <div className="game-board">
+          <Board/>
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
-      <div className="game-info">
-      <div>{/* status */}</div>
-      <ol>{/* TODO */}</ol>
-      </div>
-      </div>
-  );
+    )
   }
 }
 
